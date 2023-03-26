e22ae891d55a06b5e3678c6bc573fe63fb0b6661
Faizan <faizanfazul6@gmail.com>
2023-03-26 17:06:25 +0500

    made the hamburger menu expand and display the nav links on click

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
 const hamburger = document.getElementById("hamburger");
 const lines = document.querySelectorAll(".line");
+const nav = document.getElementById("navbar");
 
 hamburger.addEventListener("click", () => {
   lines.forEach((line) => line.classList.toggle("active"));
+  if (nav.style.display == "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
 });
