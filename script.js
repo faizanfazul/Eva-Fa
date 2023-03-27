bdd0180578d9a4d5a5d17bf3e12a22698e3e3c96
Faizan <faizanfazul6@gmail.com>
2023-03-28 02:38:02 +0500

    made responsive for tablets and also added animation on load plus made the cart button open on click

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
-const hamburger = document.getElementById("hamburger");
+const hamburger = document.getElementById("hambur");
 const lines = document.querySelectorAll(".line");
 const nav = document.getElementById("navbar");
+const cartItem = document.getElementById("cart-item");
+const cart = document.getElementById("cart");
+const cartSection = document.getElementById("cart-section");
+const xIcon = document.getElementById("x-icon");
 
 hamburger.addEventListener("click", () => {
   lines.forEach((line) => line.classList.toggle("active"));
@@ -10,4 +14,26 @@ hamburger.addEventListener("click", () => {
   } else {
     nav.style.display = "block";
   }
+  if (cartSection.style.display == "block") {
+    cartSection.style.display = "none";
+  } 
+});
+
+cart.addEventListener("click", (e) => {
+  if (nav.style.display == "block") {
+    nav.style.display = "none";
+      lines.forEach((line) => line.classList.toggle("active"));
+  }
+  cartSection.style.display = "block"
+});
+
+xIcon.addEventListener("click", (e) => {
+  cartSection.style.display = "none";
+});
+
+
+window.addEventListener("load", (e) => {
+  e.preventDefault();
+  const element = document.querySelector("html");
+  element.classList.add("show");
 });
