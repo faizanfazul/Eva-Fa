2fc3aac4f759bc2b35a7b9b660423c0bf18c0913
Faizan <faizanfazul6@gmail.com>
2023-04-16 04:07:43 +0500

    compressed images for better load time

--- slider.js
+++ slider.js
@@ -1,24 +1,3 @@
 "use strict";
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
-
-// for main slider
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  loop:true,
-  centeredSlides: true,
-  effect:"coverflow",
-  lazy:false,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+const progressCircle = document.querySelector(".autoplay-progress svg"),progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {spaceBetween: 10,loop: !0,centeredSlides: !0,effect: "coverflow",lazy: !0,autoplay: { delay: 3e3,disableOnInteraction: !1 },pagination: { el: ".swiper-pagination", clickable: !0 },navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },});
