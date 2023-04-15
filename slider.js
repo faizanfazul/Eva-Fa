cdf57a68cf7c2f53cd6c7854c262a7b295c59123
Faizan <faizanfazul6@gmail.com>
2023-04-15 07:34:36 +0500

    added the new customization on slider plus a timer

---
+++ slider.js
@@ -0,0 +1,28 @@
+"use strict";
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+
+// for main slider
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
